6236cea8073c86f5e28a1b94e3f370321ef76ee1
Paola Navas <dpaonc@outlook.es>
2020-05-17 22:24:44 +0200

    nueva rama

---
+++ src/intrototesting/Dibujante.java
@@ -0,0 +1,15 @@
+package intrototesting;
+
+public class Dibujante {
+
+	public static void dibujitoEnAscii1() {
+		System.out.println("¯\\(°_o)/¯ ");
+
+	}
+
+	public static void dibujitoEnAscii2() {
+		System.out.println("¯\\(°_°)/¯ ");
+
+	}
+
+}
